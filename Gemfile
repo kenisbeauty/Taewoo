f789f2c1e684fae80664f2449f7ebdbdbefd41db
taiwo kehinde <kenisbeauty@outlook.com>
2017-04-11 10:26:20 +0100

    devise installed

--- Gemfile
+++ Gemfile
@@ -8,6 +8,7 @@ gem 'coffee-rails', '~> 4.2'
 gem 'jquery-rails'
 gem 'turbolinks', '~> 5'
 gem 'jbuilder', '~> 2.5'
+gem 'devise'
 
 group :production do
 	gem 'rails_12factor'
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   root :to => 'static_pages#home'
 get  '/help',    to: 'static_pages#help'
   get  '/about',   to: 'static_pages#about'
